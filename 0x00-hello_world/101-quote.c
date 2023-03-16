8a7fb571a252f901cba7e1d94211eb7de944e6ff
Mohammed-Shousha <mshousha29@gmail.com>
2023-03-16 05:08:19 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main() {
+int main(void)
+{
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, msg, sizeof(msg) - 1);
-	return 1;
+	return (1);
 }
