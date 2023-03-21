b3a70451e9f13f2415f006d5c91d02f226a3733a
Mohammed-Shousha <mshousha29@gmail.com>
2023-03-21 02:13:24 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,11 @@
  * print_alphabet - Entry point
  * Return:0
  */
-void print_alphabet(void){
-	for (char c='a'; c<= 'z', c++)
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
 	_putchar('\n');
 }
