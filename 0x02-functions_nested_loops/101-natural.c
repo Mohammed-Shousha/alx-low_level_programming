8969d1e9948e5a44f8a532982bcdf5b2387e17e7
Mohammed-Shousha <mshousha29@gmail.com>
2023-03-21 05:35:48 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,17 +5,18 @@
  * of 3 or 5 below 1024
  * Return: Always 0 (Success)
  */
-void main(void)
+int main(void)
 {
 	long int sum = 0;
+	int i;
 
-	for (int i = 0; i < 1024; i++)
+	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0)
 			sum += i;
-		else if ((i % 5 == 0)
+		else if ((i % 5) == 0)
 			sum += i;
 	}
-	print("%l\n", sum);
+	printf("%l\n", sum);
 	return (0);
 }
