0b29ef4bb73c7dcf45545af8610392f34f492445
Mohammed-Shousha <mshousha29@gmail.com>
2023-03-27 15:07:43 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,13 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i, len = 0;
 
-	while (str[i] != '\0')
-	{
+	while (str[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i += 2)
 		_putchar(str[i]);
-		i += 2;
-	}
 
 	_putchar('\n');
 }
