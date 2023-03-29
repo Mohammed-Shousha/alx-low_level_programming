ea1517073193e60095ee7b23cefa25136cc8d2fa
Mohammed-Shousha <mshousha29@gmail.com>
2023-03-29 14:35:26 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string in 1337
+ * @str: string to be encoded
+ *
+ * Return: the resulting string;
+ */
+char *leet(char *str)
+{
+	int i = 0, j;
+
+	char *letters = "aAeEoOtTlL";
+	char *numbers = "4433007711";
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if(str[i] == letters[j])
+				str[i] = numbers[j];
+		}
+	}
+
+	return (str);
+}
