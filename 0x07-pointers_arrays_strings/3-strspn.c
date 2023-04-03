6552f46981f95b111c2aab77f38a5666df812b4c
Mohammed-Shousha <mshousha29@gmail.com>
2023-04-03 09:11:05 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,21 +10,27 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0, j = 0, count = 0;
+	int i, j, count = 0, match;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (accept[j] != '\0')
+		match = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
+			{
 				count++;
+				match = 1;
+			}
 
-			j++;
 		}
 
-		i++;
+		if (match == 0)
+			return (count);
+
 	}
 
-	return (count);
+	return (0);
 }
 
