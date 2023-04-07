8f4243813e4dc1dbd08dba4642be721dcee954c8
Mohammed-Shousha <mshousha29@gmail.com>
2023-04-07 14:58:02 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, num, sum = 0;
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
@@ -23,10 +23,8 @@ int main(int argc, char *argv[])
 			}
 		}
 
-		num = atoi(argv[i]);
 
-		if (num > 0)
-			sum += num;
+		sum += atoi(argv[i]);
 	}
 
 
