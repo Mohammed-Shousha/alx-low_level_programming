6395e0d70e30e39ba9a979d4e0660a7fab8a3c0d
Mohammed-Shousha <mshousha29@gmail.com>
2023-04-19 12:39:28 -0700

    2-int_index.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,8 +2,8 @@
 #ifndef FUNC_P
 #define FUNC_P
 
-
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
