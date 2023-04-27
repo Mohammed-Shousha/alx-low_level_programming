19754efdf54872b52f2c88e15a2993ad5466e65e
Mohammed-Shousha <mshousha29@gmail.com>
2023-04-27 04:25:09 -0700

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+section .data
+    msg db 'Hello, Holberton',0x0A,0
+
+section .text
+    global main
+
+    extern printf
+
+main:
+    push rbp
+    mov rbp, rsp
+    
+    lea rdi, [msg]
+    mov eax, 0
+    call printf
+    
+    mov rsp, rbp
+    pop rbp
+    
+    mov eax, 0
+    ret
