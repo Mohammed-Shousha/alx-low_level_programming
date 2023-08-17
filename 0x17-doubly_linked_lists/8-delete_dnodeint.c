025027cb2da115a5e0339a55a481d5943f320c03
mohammed shousha <mshousha29@gmail.com>
2023-08-17 05:20:37 -0700

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,40 +10,40 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current_node, *prev_node;
-    unsigned int i;
+	dlistint_t *current_node, *prev_node;
+	unsigned int i;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    current_node = *head;
+	current_node = *head;
 
-    if (index == 0)
-    {
-        *head = current_node->next;
+	if (index == 0)
+	{
+		*head = current_node->next;
 
-        if (*head)
-            (*head)->prev = NULL;
+		if (*head)
+			(*head)->prev = NULL;
 
-        free(current_node);
-        return (1);
-    }
+		free(current_node);
+		return (1);
+	}
 
-    for (i = 0; current_node && i < index; i++)
-    {
-        prev_node = current_node;
-        current_node = current_node->next;
-    }
+	for (i = 0; current_node && i < index; i++)
+	{
+		prev_node = current_node;
+		current_node = current_node->next;
+	}
 
-    if (current_node == NULL)
-        return (-1);
+	if (current_node == NULL)
+		return (-1);
 
-    prev_node->next = current_node->next;
+	prev_node->next = current_node->next;
 
-    if (current_node->next)
-        current_node->next->prev = prev_node;
+	if (current_node->next)
+		current_node->next->prev = prev_node;
 
-    free(current_node);
+	free(current_node);
 
-    return (1);
+	return (1);
 }
